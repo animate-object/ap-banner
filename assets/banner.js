03dea02c5a7073ce6ec4a7b1e1688bbd5c1aa092
animate-object <alexander.f.prinsen@gmail.com>
2020-04-02 14:49:39 -0500

    banner basically working

--- assets/banner.js
+++ assets/banner.js
@@ -1,39 +1,73 @@
 // goals
 // x banner component that spreads across the screen
-// - banner can be expanded for more detailed content,
+// x banner can be expanded for more detailed content,
 //   including links
-// - banner content is provided by consumer
+// x banner content is provided by consumer
 // - banner is disimissable, and won't appear again on page load once dismissed
 
-const BANNER_TEMPLATE_NAME = "ap-banner-template";
+const BANNER_TEMPLATE_ID = "ap-banner-template";
+const LOCAL_STORAGE_KEY_PREFIX = "ap-banner-cached-state";
+
+const storageKey = item => `${LOCAL_STORAGE_KEY_PREFIX}.${item}`;
+
 class ApBanner extends HTMLElement {
   constructor() {
     super();
     this.shadow = this.attachShadow({ mode: "closed" });
-    this.setState({ expanded: false });
-    this.firstDraw();
+    this.setState({
+      expanded: false,
+      hidden: localStorage.getItem(storageKey("hidden")) === "true"
+    });
+    if (!this.getState().hidden) {
+      this.firstDraw();
+      this.update();
+    }
   }
 
   firstDraw = () => {
-    console.log({ at: "draw", state: this.state });
     // container spans the width of the viewport
-    this.template = this._template();
-
-    const banner = document.createElement("div");
-    banner.setAttribute("class", "banner");
-
-    this.arrow = this.arrow();
-    banner.appendChild(this.arrow);
-    banner.appendChild(this.title());
-    banner.appendChild(template.content.cloneNode(true));
+    this.banner_template = this._banner_template();
 
     this.shadow.appendChild(this.styles());
-    this.shadow.appendChild(banner);
+    this.templateFragment = this.banner_template.content.cloneNode(true);
+    this.bannerEl = this.templateFragment.firstChild;
+    this.shadow.appendChild(this.templateFragment);
   };
 
   update = newState => {
     this.setState(newState);
-    this.updateArrowEl(this.arrow);
+
+    if (this.state.hidden) {
+      while (this.shadow.firstChild) {
+        this.shadow.removeChild(this.shadow.lastChild);
+      }
+      return;
+    }
+
+    if (!this.bannerEl) {
+      return;
+    }
+
+    const titleEl = this.bannerEl.getElementsByClassName("expansion-handle")[0];
+    if (titleEl) {
+      this.expandOnClick(titleEl);
+    }
+
+    const closeForeverEl = this.bannerEl.getElementsByClassName(
+      "close-link"
+    )[0];
+    if (closeForeverEl) {
+      this.closeForeverOnClick(closeForeverEl);
+    }
+
+    const arrowEl = this.bannerEl.getElementsByClassName("arrow")[0];
+    if (arrowEl) {
+      this.updateArrowEl(arrowEl);
+    }
+    const moreInfoEl = this.bannerEl.getElementsByClassName("more-info")[0];
+    if (moreInfoEl) {
+      this.updateMoreInfo(moreInfoEl);
+    }
   };
 
   setState = newState => {
@@ -60,29 +94,93 @@ class ApBanner extends HTMLElement {
     const arrow = document.createElement("span");
     arrow.textContent = ">";
     arrow.setAttribute("class", "arrow");
-    this.updateArrowEl(arrow);
     return arrow;
   };
 
+  sourceCodeLink = () => {
+    const anchor = document.createElement("a");
+    anchor.classList.add("action-link");
+    anchor.classList.add("source-link");
+    anchor.textContent = "view source code";
+    anchor.setAttribute("href", this.getAttribute("source"));
+    anchor.setAttribute("target", "_blank");
+    return anchor;
+  };
+
+  closeForeverLink = () => {
+    const anchor = document.createElement("a");
+    anchor.classList.add("action-link");
+    anchor.classList.add("close-link");
+    anchor.textContent = "don't show me this again";
+    return anchor;
+  };
+
   updateArrowEl = el => {
-    console.log(el, this.state);
     if (this.state.expanded) {
       el.classList.add("expanded");
     } else {
       el.classList.remove("expanded");
     }
+  };
+
+  expandOnClick = el => {
     el.onclick = () => this.update({ expanded: !this.state.expanded });
   };
 
-  _template = () => {
-    const template = document.createElement("template");
-    template.setAttribute("id", BANNER_TEMPLATE_NAME);
-    const contentSlot = document.createElement("slot");
-    contentSlot.setAttribute("name", "ap-banner-content");
-    template.appendChild(contentSlot);
+  closeForeverOnClick = el => {
+    el.onclick = () => {
+      localStorage.setItem(storageKey("hidden"), "true");
+      this.update({ hidden: true });
+    };
+  };
 
-    // by adding this template to the document, is made available for later use
-    document.body.appendChild(this.template);
+  updateMoreInfo = el => {
+    if (this.state.expanded) {
+      el.classList.add("expanded");
+    } else {
+      el.classList.remove("expanded");
+    }
+  };
+
+  _banner_template = () => {
+    const template = document.createElement("template");
+    template.setAttribute("id", BANNER_TEMPLATE_ID);
+
+    const banner = document.createElement("div");
+    banner.setAttribute("class", "banner");
+
+    const primaryContent = document.createElement("span");
+    primaryContent.setAttribute("class", "expansion-handle");
+    primaryContent.appendChild(this.arrow());
+    primaryContent.appendChild(this.title());
+
+    const linkContainer = document.createElement("links");
+    linkContainer.setAttribute("class", "link-container");
+    linkContainer.appendChild(this.sourceCodeLink());
+    linkContainer.appendChild(this.closeForeverLink());
+
+    const titleContainer = document.createElement("div");
+    titleContainer.setAttribute("class", "title-container");
+    titleContainer.appendChild(primaryContent);
+    titleContainer.appendChild(linkContainer);
+
+    banner.appendChild(titleContainer);
+
+    const moreInfo = document.createElement("div");
+    moreInfo.setAttribute("class", "more-info");
+    const infoSlot = document.createElement("slot");
+    infoSlot.setAttribute("name", "more-info");
+    moreInfo.appendChild(infoSlot);
+    banner.appendChild(moreInfo);
+
+    // important note: when dynamically creating a <template /> (i.e. in JS),
+    // children must be attached via the content property
+    // or they will not render after cloning
+    template.content.appendChild(banner);
+
+    // adding the template to the document sort of 'registers' it for later use
+    // but does not render it.
+    document.body.appendChild(template);
 
     return template;
   };
